f88f62950e850f27e2a37e8f37b1a7c025f6802c
Live session user <agageldi@gmail.com>
2018-11-17 21:17:41 +0100

    Server Nodejs+pg Server

---
+++ Server/server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var pg = require("pg");
+var app = express();
+
+var connectionString = "postgres://user:user@localhost:5432/aga";
+
+app.get('/', function (req, res, next) {
+    pg.connect(connectionString,function(err,client,done) {
+       if(err){
+           console.log("not able to get connection "+ err);
+           res.status(400).send(err);
+       }
+       client.query('SELECT * FROM student where id = $1', [1],function(err,result) {
+           done(); // closing the connection;
+           if(err){
+               console.log(err);
+               res.status(400).send(err);
+           }
+           res.status(200).send(result.rows);
+       });
+    });
+});
+
+app.get('/a', function (req, res, next) {
+    pg.connect(connectionString,function(err,client,done) {
+       if(err){
+           console.log("not able to get connection "+ err);
+           res.status(400).send(err);
+       }
+       client.query('SELECT *,ST_ASTEXT(geom) as Coordinates FROM places', function(err,result) {
+           done(); // closing the connection;
+           if(err){
+               console.log(err);
+               res.status(400).send(err);
+           }
+           console.log(result);
+           res.status(200).send(result.rows[0]);
+       });
+    });
+});
+
+
+app.listen(4000, function () {
+    console.log('Server is running.. on Port 4000');
+});
