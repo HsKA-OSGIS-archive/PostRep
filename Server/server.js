8c9ff08915ccb411bff83c4af3ebdfee5f446388
user <agageldi@gmail.com>
2018-11-21 11:56:47 +0300

    change in instruction

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var pg = require("pg");
 var app = express();
 
-var connectionString = "postgres://user:user@localhost:5432/aga";
+var connectionString = "postgres://user:user@localhost:5432/saag_geo";
 
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -16,13 +16,29 @@ app.get('/', function (req, res, next) {
            console.log("not able to get connection "+ err);
            res.status(400).send(err);
        }
-       client.query('SELECT * FROM student where id = $1', [1],function(err,result) {
+       client.query('SELECT * FROM roads', function(err,result) {
            done(); // closing the connection;
            if(err){
                console.log(err);
                res.status(400).send(err);
            }
-           res.status(200).send(result.rows);
+           res.status(200).send(result.rows[1]);
+       });
+    });
+});
+app.get('/b', function (req, res, next) {
+    pg.connect(connectionString,function(err,client,done) {
+       if(err){
+           console.log("not able to get connection "+ err);
+           res.status(400).send(err);
+       }
+       client.query('SELECT ST_ASTEXT(geom) as Coordinates FROM roads', function(err,result) {
+           done(); // closing the connection;
+           if(err){
+               console.log(err);
+               res.status(400).send(err);
+           }
+           res.status(200).send(result.rows[1]);
        });
     });
 });
