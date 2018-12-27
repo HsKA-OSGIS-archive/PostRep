968d86852747e7a90d23ce5e47bf85f14575f39c
Live session user <agageldi@gmail.com>
2018-12-27 14:56:46 +0000

    simple function

--- aga.sql
+++ aga.sql
@@ -1,3 +1,9 @@
+--Select * from stg.rad_data;
+
+--Select * from proc.in_data;
+
+--CREATE INDEX ON proc.in_data USING gist(geom);
+
 --DROP FUNCTION proc.func;
 
 CREATE or REPLACE FUNCTION PROC.FUNC (myDate timestamp)
@@ -5,11 +11,28 @@ RETURNS TABLE(val double precision,Datum timestamp,geom geometry) AS
 $$
 SELECT inData.val AS val,myDate::timestamp AS date, myVoronoi.geom
 FROM 	(SELECT  (ST_Dump(ST_CollectionExtract(ST_VoronoiPolygons(ST_Collect(DISTINCT geom)) ,3))).geom
-	     FROM proc.in_data
-	     WHERE date = myDate) AS myVoronoi,
-       proc.in_data AS inData
+	FROM proc.in_data
+	WHERE date = myDate) AS myVoronoi,proc.in_data AS inData
 WHERE inData.date = myDate
   AND ST_intersects(inData.geom,myVoronoi.geom)
 $$LANGUAGE SQL;
 
-SELECT * from proc.func('2015-10-12 21:50:00')
+--select date from proc.voronoi;
+--Select date from proc.voronoi where date = '2015-10-12 21:50:00';
+
+SELECT * from proc.func('2015-10-12 21:50:00');
+
+--DROP FUNCTION proc.functin;
+
+CREATE or REPLACE FUNCTION PROC.FUNCTIN (myDate timestamp)
+RETURNS TABLE(val double precision,Datum timestamp,geom geometry) AS
+$$
+SELECT inData.val AS val,myDate::timestamp AS date, myVoronoi.geom
+FROM 	(SELECT  (ST_Dump(ST_CollectionExtract(ST_DelaunayTriangles(ST_Collect(DISTINCT geom)) ,3))).geom
+	FROM proc.in_data
+	WHERE date = myDate) AS myVoronoi,proc.in_data AS inData
+WHERE inData.date = myDate
+  AND ST_intersects(inData.geom,myVoronoi.geom)
+$$LANGUAGE SQL;
+
+SELECT * from proc.functin('2015-10-12 21:50:00');
